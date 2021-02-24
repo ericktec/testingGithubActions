a020d8e03e1531dc69e2be10b55ba3181018c2b7
Erick <erick_tec@outlook.com>
2021-02-24 14:38:59 -0600

    test actions

---
+++ components/Navigation.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+const Navigation = () => {
+    return (
+        <ul>
+            <li>
+                <Link href="/">
+                    <a>Index</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Navigation;
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+
+const About = () => {
+    return (
+    <>
+        <Navigation/>
+        <h1>About</h1>
+    </>
+    )
+}
+
+
+export default About;
